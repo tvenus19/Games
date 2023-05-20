d5ac940c80f71552a4848fc2d2e7aeb96c36b5c3
tvenus19 <teo-v@hotmail.com>
2023-05-20 23:46:33 +0200

    pong revised2

--- Pong/assets.py
+++ Pong/assets.py
@@ -1,4 +1,10 @@
+from pathlib import Path
 import simpleaudio as sa
 
-# Load sound
-bounce_sound = sa.WaveObject.from_wave_file("bounce.wav")
+# Path to the directory of the current script
+script_dir = Path(__file__).parent
+
+# Path to the sound file
+sound_file = script_dir / "bounce.wav"
+
+bounce_sound = sa.WaveObject.from_wave_file(str(sound_file))
--- Pong/main.py
+++ Pong/main.py
@@ -1,24 +1,36 @@
-from config import window
+# main.py
+
+import turtle
+from game_constants import *
 from game_objects import Ball, Slider, ScoreDisplay
-from game_functions import move_ball, bounce_ball, score_point
-from input_handlers import slider_a_up, slider_a_down, slider_b_up, slider_b_down
+from input_handlers import setup_input_handlers
+from game_functions import move_ball, check_boundaries, check_score, check_collision
+
+# Create the window
+window = turtle.Screen()
+window.title("Venus Pong")
+window.bgcolor(WINDOW_BG_COLOR)
+window.setup(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+window.tracer(0)
+
+# Create game objects
+ball = Ball()
+slider_a = Slider(SLIDER_A_START_POS)
+slider_b = Slider(SLIDER_B_START_POS)
+score_display = ScoreDisplay()
+
+# Setup input handlers
+setup_input_handlers(window, slider_a, slider_b)
+
+# Initial scores
+score_a = SCORE_A_START
+score_b = SCORE_B_START
 
 # Main game loop
 while True:
     window.update()
 
-    # Move the ball
     move_ball(ball)
-
-    # Bounce the ball
-    bounce_ball(ball, slider_a, slider_b)
-
-    # Score a point
-    score_point(ball, slider_a, slider_b, score_display)
-
-    # Handle input
-    window.listen()
-    window.onkeypress(slider_a_up, "w")
-    window.onkeypress(slider_a_down, "s")
-    window.onkeypress(slider_b_up, "Up")
-    window.onkeypress(slider_b_down, "Down")
+    check_boundaries(ball)
+    score_a, score_b = check_score(ball, score_a, score_b, score_display)
+    check_collision(ball, slider_a, slider_b)
